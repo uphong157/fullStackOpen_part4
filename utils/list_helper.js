8ff548a6d3b268b8514086ae99c10fb9244536ec
uphong157 <uphong157@gmail.com>
2023-06-17 21:41:44 -0700

    Complete exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,8 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = () => {
+const totalLikes = (blogs) => {
+  const addAllLikes = (sum, blog) => {
+    return sum + blog.likes
+  }
 
+  return blogs.reduce(addAllLikes, 0)
 }
 
 module.exports = {
