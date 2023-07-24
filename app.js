d891cc0d24b46fd3e9b8134646d79214b3a77057
uphong157 <uphong157@gmail.com>
2023-07-23 21:27:28 -0700

    Complete exercise 4.18

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
